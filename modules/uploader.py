01c7d0ca3d8d512d0a2266a8696c93d80e6a0cb2
Do Jun Kim <99for0@brich.co.kr>
2025-11-11 11:22:41 +0900

    re

--- modules/uploader.py
+++ modules/uploader.py
@@ -42,7 +42,7 @@ class BeeflowUploader:
     def init_driver(self):
         """Chrome 드라이버 초기화"""
         options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
+        # options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--window-size=1920,1080')
